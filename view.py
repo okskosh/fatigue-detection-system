e0746ed7fb29de3cb89939ec49ba6fb5c8c75b0e
Oks Kosh <oksana.pavletsova.00@bk.ru>
2022-05-10 11:03:08 +0300

    feat: gui implemented

---
+++ view.py
@@ -0,0 +1,27 @@
+import tkinter as tki
+from detection import process, with_alert
+
+
+def app_view():
+    # initialize the root window 
+    root = tki.Tk()
+    root.title("Fatigue Detection System")
+    root.geometry('300x250')
+
+    # button, that when pressed, exits the app
+    exit_btn = tki.Button(root, text="Exit", bg="#BDD9BF", command=root.destroy)
+    exit_btn.pack(side="bottom", fill="both", expand="yes", padx=10, pady=10)
+
+    # button, that when pressed, runs the app in drive mode (with sound alert)
+    drive_btn = tki.Button(root, text="Driving mode", bg="#BDD9BF", command=with_alert)
+    drive_btn.pack(side="bottom", fill="both", expand="yes", padx=10, pady=10)
+
+    # button, that when pressed, runs the app in test mode (no sound alert)
+    test_btn = tki.Button(root, text="Testing mode", bg="#BDD9BF", command=process)
+    test_btn.pack(side="bottom", fill="both", expand="yes", padx=10, pady=10)
+
+    # create label on window
+    label = tki.Label(root, text="Fatigue Detection", bg="#2E4052", fg='white', height=2)
+    label.pack(side="bottom", fill="both", expand="yes", padx=10, pady=10)    
+
+    root.mainloop()
